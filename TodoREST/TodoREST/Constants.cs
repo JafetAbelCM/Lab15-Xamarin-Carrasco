ef519ab7c1388e7f93bdd4710b9bf3893aa03d04
Jafet Carrasco <jafet.carrasco@tecsup.edu.pe>
2021-06-20 19:14:00 -0500

    Add project files.

---
+++ TodoREST/TodoREST/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        //public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
+        public static string RestUrl = "http://colegioquimico-001-site6.dtempurl.com/api/todoitems/{0}";
+
+
+    }
+}
---
+++ TodoREST/TodoREST/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoREST.Models;
+
+namespace TodoREST.Data
+{
+	public interface IRestService
+	{
+		Task<List<TodoItem>> RefreshDataAsync();
+
+		Task SaveTodoItemAsync(TodoItem item, bool isNewItem);
+
+		Task DeleteTodoItemAsync(string id);
+	}
+}
+
